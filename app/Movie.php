9a031fda422881b0346bca5f8f6ca8685d3e73a1
Ivan Kostadinović <ivan.k@vivifyideas.com>
2017-10-25 19:20:57 +0200

    zadatak 2

--- app/Movie.php
+++ app/Movie.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
 
+    protected $fillable = ['name', 'director', 'image_url', 'duration', 'release_date', 'genres'];
+
     protected $casts = [
         'genres' => 'array',
     ];
