7d86a06234f1d44ab9a3cf9dbe857458851a2125
Ivan Kostadinović <ivan.k@vivifyideas.com>
2017-10-25 20:38:33 +0200

    zadatak 5

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -12,9 +12,15 @@ class MoviesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Movie::all();
+        $name = $request->query('name');
+
+        if ($request) {
+            return Movie::where('name', 'like', '%' . $name . '%')->get();
+        } else {
+            return Movie::all();
+        }
     }
 
     /**
